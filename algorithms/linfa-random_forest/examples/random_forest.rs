522943a42a1fa01539daf979087dc90ffc58258e
Jacek <jaceman44@gmail.com>
2023-04-26 23:11:58 +0200

    Added dummy, test code

--- algorithms/linfa-random_forest/examples/random_forest.rs
+++ algorithms/linfa-random_forest/examples/random_forest.rs
@@ -1,19 +1,54 @@
 use ndarray_rand::rand::SeedableRng;
 use rand::rngs::SmallRng;
+// use std::fmt::Debug;
+use linfa_random_forest::{Forest, ForestTwo, Lasowy, Tree};
 
-use linfa_random_forest::{Forest};
 
+fn fun_test(las: &dyn Lasowy){
+    las.print_forest();
+}
+
+
+fn fun_test2(mask: String) -> Tree {
+    Tree {
+        mask,
+        nsamples: 2,
+    }
+}
 fn main() {
     println!("Main running...");
 
     // load Iris dataset
     let mut rng = SmallRng::seed_from_u64(42);
 
-    let (train, test) = linfa_datasets::iris()
+    let (_train, _test) = linfa_datasets::iris()
         .shuffle(&mut rng)
         .split_with_ratio(0.8);
 
-    let simple_forest: Forest::params();
- 
+    // let simple_forest: Forest::params();
+
+    let forest: Forest = Forest;
+    fun_test(&forest);
+
+    
+    let forest_two = ForestTwo::forest_two_test();
+
+    fun_test(&forest_two);
+    // forest.printForest();
+
+    // let drzewo: Tree;
+    let a= fun_test2(String::from("DUPA"));
+
+
+    // let forest_test= forest_two.create_forest();
+
+    // let test: ForestTwo =  forest_two.forest_two_test();
+
+    // println!("{:#?}", forest_test);
+    
+    println!("{:#?}", forest_two);
+
+    println!("String: {}", a.mask);
+    
     
 }
--- algorithms/linfa-random_forest/src/random_forest/algorithm.rs
+++ algorithms/linfa-random_forest/src/random_forest/algorithm.rs
@@ -1,33 +1,96 @@
 //! Random forest based on linear decision trees
 //!
 
-use linfa_trees::{DecisionTree, Result, SplitQuality};
+use linfa_trees::{DecisionTree, Result, SplitQuality, DecisionTreeParams};
+// use codec::vec;
 
 pub struct Tree {
-    mask: Vec<bool>,
-    nsamples: usize,
+    pub mask: String,
+    pub nsamples: u8,
 }
 
-pub struct Forest{
-    forest: u32,
-    // forest: Vec<Tree>,
+// pub struct Forest{
+//     forest: u32,
+//     // forest: Vec<Tree>,
+// }
+
+pub trait ForestManager {
+    fn params() -> Forest;
+}
+
+// impl ForestManager for Forest {
+//     /// Defaults are provided if the optional parameters are not specified:
+//     /// * `split_quality = SplitQuality::Gini`
+//     /// TODO
+//     /// * `max_depth = None`
+//     /// * `min_weight_split = 2.0`
+//     /// * `min_weight_leaf = 1.0`
+//     /// * `min_impurity_decrease = 0.00001`
+//     // Violates the convention that new should return a value of type `Self`
+//     // #[allow(clippy::new_ret_no_self)]
+//     fn params() -> Forest {
+//         Forest { forest: (32), }
+//     }
+// }
+pub trait Lasowy {
+    fn print_forest(&self);
 }
 
 
-impl Forest {
-    /// Defaults are provided if the optional parameters are not specified:
-    /// * `split_quality = SplitQuality::Gini`
-    /// TODO
-    /// * `max_depth = None`
-    /// * `min_weight_split = 2.0`
-    /// * `min_weight_leaf = 1.0`
-    /// * `min_impurity_decrease = 0.00001`
-    // Violates the convention that new should return a value of type `Self`
-    #[allow(clippy::new_ret_no_self)]
-    pub fn params() -> Self {
-        Forest { forest: (32), }
+pub struct Forest;
+// {
+//     forest_name: String,
+//     email: u64,
+// }
+
+impl Lasowy for Forest {
+    fn print_forest(&self){
+        println!("I'm in forest!");
     }
 }
+
+#[derive(Debug)]
+pub struct ForestEx{
+    forest_name: String,
+    email: u64,
+    vector: DecisionTreeParams<f64, usize>,
+}
+
+#[derive(Debug)]
+pub struct ForestTwo(ForestEx);
+
+impl Lasowy for ForestTwo {
+    fn print_forest(&self){
+        println!("I'm in forestTwo!");
+    }
+}
+
+impl ForestTwo{
+    pub fn forest_two_test(&self) -> Self{
+        Self(ForestEx{
+            forest_name: String::from("DUPA INIT"),
+            email: (453),
+            vector: self.create_forest(),
+        })
+    }
+
+    pub fn create_forest(&self) -> DecisionTreeParams<f64, usize> {
+        DecisionTree::<f64,usize>::params()
+    }
+
+    pub fn create_forest_vector(&self){
+
+    }
+}
+
+
+
+// DecisionTree::params()
+//         .split_quality(SplitQuality::Gini)
+//         .max_depth(Some(100))
+//         .min_weight_split(1.0)
+//         .min_weight_leaf(1.0)
+//         .fit(&train)?;
 
 // impl Forest{
 //     // Return test
@@ -37,6 +100,8 @@ impl Forest {
 // }
 
 
+
+// 1. Init one tree
 // TODO:
 //  1. Get train data.
 //  2. Get number of trees in forest.
@@ -50,3 +115,11 @@ impl Forest {
 // .min_weight_split(1.0)
 // .min_weight_leaf(1.0)
 // .fit(&train)?;
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn autotraits() {
+    }
+}
